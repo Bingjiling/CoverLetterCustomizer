cd72890f96268c798dea188079c84d4d3ca73120
Yanyu Zheng <yz2690@columbia.edu>
2016-03-05 19:42:02 -0500

    update MakeCL.R

--- lib/MakeCL.R
+++ lib/MakeCL.R
@@ -8,6 +8,13 @@ dir.create("Encore")
 setwd("./Encore")
 output = "PlayCoverLeter.rtf"
 rtf = RTF(output)
-add = gsub(","," \n ",compInfo$Address)
+add = gsub(", ","\n",compInfo$Address)
+rtf = rtf %>%
+  addParagraph("125 W 109TH ST\nAPT 45C\n
+New York, NY 10027\n3477261047\nyz2690@columbia.edu\n\n") %>%
+  done()
 
+  endParagraph() %>%
+  addParagraph("125 W 109TH ST\nAPT 45C\n
+New York, NY 10027\n3477261047\nyz2690@columbia.edu\n\n")
 done(rtf)
