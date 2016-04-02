c18a6b4dfc74a1288a37f65bb2a31d597c59fc58
Yanyu Zheng <yz2690@columbia.edu>
2016-04-02 00:46:16 -0400

    applyied for two more

--- Main.R
+++ Main.R
@@ -2,8 +2,17 @@
 setwd("~/living/Applications")
 lib = c("googlesheets","dplyr","rtf")
 lapply(lib,library,character.only = TRUE)
+source("./CoverLetterCustomizer/lib/MakeCL.R")
+getwd()
 ## Get the spreadsheet
 sheet = gs_title("Applications")
 Info = gs_read_csv(sheet)
 ## Compose the cover letter
-makeCL("Test",Info,sheet)
+## 3.30
+#makeCL("Analytic Partners",Info,sheet)
+## 4.1
+#makeCL("Dextro Software Systems Inc",Info,sheet)
+makeCL("Encore Capital Group",Info,sheet)
+
+
+
