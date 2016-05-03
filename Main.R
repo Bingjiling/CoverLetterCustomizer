90328f51de7b6b2fa361492025515f3830b57fb1
Yanyu Zheng <yz2690@columbia.edu>
2016-05-03 19:46:14 -0400

    minor change

--- Main.R
+++ Main.R
@@ -12,7 +12,13 @@ Info = gs_read_csv(sheet)
 #makeCL("Analytic Partners",Info,sheet)
 ## 4.1
 #makeCL("Dextro Software Systems Inc",Info,sheet)
-makeCL("Encore Capital Group",Info,sheet)
+#makeCL("Encore Capital Group",Info,sheet)
+#makeCL("Rang Technologies Inc.",Info,sheet)
+#makeCL("MIO Partners Inc.", Info, sheet)
+#makeCL("Optimal Strategix Group ", Info, sheet)
+#makeCL("C1", Info, sheet)
+makeCL("Group M", Info, sheet)
 
-
-
+# sheet = gs_title("STATW4249_001_2016.xls")
+# Info = gs_read_csv(sheet)
+# names = Info$Name
